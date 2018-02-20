c424936f8dc9c3cc94e040102124172ba4db200c
Sherlock Holo <sherlockya@gmail.com>
2018-02-20 10:29:28 +0800

    Add timeout arg

--- src/main/kotlin/holoDownloader/main/Main.kt
+++ src/main/kotlin/holoDownloader/main/Main.kt
@@ -8,18 +8,20 @@ fun main(args: Array<String>) {
     val parser = CmdParser()
     parser.addParameter("-o")
     parser.addParameter("-n")
+    parser.addParameter("--timeout")
     parser.parse(args)
 
     val link = parser.link
-    val threadNum =
-            if (parser.getParameter("-n") != null) parser.getParameter("-n")!!.toInt()
-            else 16
+
+    val threadNum = parser.getParameter("-n")?.toInt() ?: 16
+
+    val timeout = parser.getParameter("timeout")?.toLong() ?: 0
 
     if (link == null) {
         println("no download link or error download link")
         System.exit(1)
     }
 
-    val downloader = DownloadManager(link!!, threadNum, parser.getParameter("-o"), 500)
+    val downloader = DownloadManager(link!!, threadNum, parser.getParameter("-o"), timeout, 500)
     downloader.download()
 }
